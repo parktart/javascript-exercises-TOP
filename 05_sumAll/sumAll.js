a266c393c4b30104c48df93f7b8c580d9723001f
parker <parkerschellenberg@gmail.com>
2023-03-09 16:24:46 -0600

    Complete 05-sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(low, high) {
+  if (typeof low !== 'number' || typeof high !== 'number') return 'ERROR'
+  if (low < 0 || high < 0) return 'ERROR'
+  if (low === high) return low
 
+  let result = 0;
+  for (let i = Math.min(low, high); i <= Math.max(low, high); i++) {
+    result = result + i;
+  }
+  return result;
 };
 
 // Do not edit below this line
