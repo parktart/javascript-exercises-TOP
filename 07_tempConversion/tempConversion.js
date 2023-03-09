593bce518ab6c95850408fc07aa3261597881f43
parker <parkerschellenberg@gmail.com>
2023-03-09 17:02:45 -0600

    Complete 07-tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degF) {
+  degC = (degF - 32) * 5/9;
+  return parseFloat(degC.toFixed(1));   // round to 1 decimal
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degC) {
+  degF = (degC * 9/5 + 32);
+  return parseFloat(degF.toFixed(1));   // round to 1 decimal
 };
 
 // Do not edit below this line
